d88df45816367163ba446b654efe1e7096a3367b
tepper-d <104649969+tepper-d@users.noreply.github.com>
2022-11-18 15:28:07 -0800

    lesson 2: create vars

---
+++ blackjack.js
@@ -0,0 +1,41 @@
+/*
+CIS 124: Introduction to JavaScript
+Module 6: Scrimba Exercises
+Build a Blackjack game, https://scrimba.com/playlist/p3py7U7
+Dominique Tepper, 18NOV2022
+*/
+
+/* LESSON 2: Add vars firstCard, secondCard, sum
+    A. firstCard and secondCard - set value to random number between 2-11
+    B. sum = firstCard + secondCard
+Tepper, 18NOV2022*/
+
+let firstCard = 0;
+let secondCard = 0;
+let sum = 0;
+
+// firstCard
+const kort1 = () => {
+    let korten1 = Math.floor(Math.random() * 11) + 1;
+    firstCard = korten1;
+}
+
+// secondCard
+const kort2 = () => {
+    let korten2 = Math.floor(Math.random() * 11) + 1;
+    secondCard = korten2;
+}
+
+// sum
+const cardSums = () => {
+    let pairSum = firstCard + secondCard;
+    sum = pairSum;
+}
+
+/* LESSON 3: if-else Conditionals
+
+Tepper, 18NOV2022 */
+
+const validateSums = () => {
+    
+}
