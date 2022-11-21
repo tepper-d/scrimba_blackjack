5bf56fd682de1b7ab29b30b23997a4d54922bb17
tepper-d <104649969+tepper-d@users.noreply.github.com>
2022-11-21 11:44:00 -0800

    L18-19 custom fxns. code cleanup

--- blackjack.js
+++ blackjack.js
@@ -7,15 +7,6 @@ Dominique Tepper, 18NOV2022
 
 "use strict";
 
-/* GLOBAL VARS. Tepper, 18NOV2022 */
-let firstCard = 0;
-let secondCard = 0;
-let thirdCard = 0;
-let sum = 0;
-let hasBlackJack = false;
-let isAlive = true; // Lesson 8a
-let message = ""; // 10a
-
 /* LESSON 2: Add vars firstCard, secondCard, sum
         a. firstCard and secondCard - set value to random number between 2-11
         b. sum = firstCard + secondCard
@@ -43,145 +34,119 @@ let message = ""; // 10a
         b. display cards values in cardsEl.textContent
     LESSON 17. New card button
         Create newCard() that logs "Drawing a new cad form the deck"
+    LESSON 18. Add to the sum when newCard is clicked
+        a. create a card var (2-11)
+        b. add new card to var sum
+        c. call startGame()
+    LESSON 19. Rename startGame()
+        create a new function called startGame() that calls renderGames()
 Tepper, 18NOV2022*/
 
-/* TEST SPACE. Tepper, 21NOV2022 */
-const startGame = () => {
-    let messageEl = document.getElementById("message-el"); // 14a
-    let sumEl = document.getElementById("sum-el"); // 15a
-    let cardsEl = document.getElementById("cards-el"); // 16a
+/* GLOBAL VARS. Tepper, 18NOV2022 */
+let messageEl = document.getElementById("message-el"); // 14a
+let sumEl = document.getElementById("sum-el"); // 15a
+let cardsEl = document.getElementById("cards-el"); // 16a
 
-    let cardOne = Math.floor(Math.random() * 11) + 1;
-    let cardTwo = Math.floor(Math.random() * 11) + 1;
-    let cardTotal = cardOne + cardTwo;
-    let blackjack = false;
-    let inGame = true;
-    let msg = "";
+let firstCard = 0;
+let secondCard = 0;
+let thirdCard = 0;
+let sum = 0;
+let hasBlackJack = false;
+let isAlive = true; // Lesson 8a
+let message = ""; // 10a
+let hasThirdCard = false;
 
-    console.log("btn click");
-    if (cardTotal <= 20) {
-        msg += "You have " + cardTotal + ". Do you want to draw a new card?";
-    }
-    else if (cardTotal === 21) {
-        msg += "You have blackjack!";
-        blackjack = true;
+
+/* GAME RESET. Tepper, 21NOV2022 */
+const reset = () => {
+    firstCard = 0;
+    secondCard = 0;
+    thirdCard = 0;
+    sum = 0;
+    hasBlackJack = false;
+    isAlive = true;
+    message = "";
+    hasThirdCard = false;
+}
+
+
+/* GENERATE CARDS. Tepper, 21NOV2022 */
+
+// first & secondCard random generator for START GAME
+const generateRandomCards = () => {
+    console.log("Generating random first and second cards...");
+
+    let randomCardOne = Math.floor(Math.random() * 11) + 1;
+    let randomCardTwo = Math.floor(Math.random() * 11) + 1;
+
+    firstCard = randomCardOne;
+    secondCard = randomCardTwo;
+    sum = firstCard + secondCard;
+
+    sumEl.textContent = "Sum:" + sum; // 15b
+    cardsEl.textContent = "Cards: " + firstCard + ", " + secondCard; // 16b
+}
+
+// thirdCard random generator
+const generateThirdCard = () => {
+    console.log("Generating new card from deck...");
+
+    let randomCardThree = Math.floor(Math.random() * 11) + 2; // 18a
+    
+    thirdCard = randomCardThree;
+    hasThirdCard = true;
+    let sumThreeCards = sum + thirdCard;
+
+    sum = sumThreeCards;
+
+    sumEl.textContent = "Sum:" + sum; // 15b
+    cardsEl.textContent = "Cards: " + firstCard + ", " + secondCard + ", " + thirdCard; // 16b
+}
+
+
+/* CARD SUM CHECK AND DRAW PROMPTS. Tepper, 21NOV2022 */
+const checkCardSums = () => {
+    if ((isAlive === true) && (hasThirdCard === false)) {
+        if (sum === 21) { // player automatically wins
+            message = "You have blackjack!";
+            hasBlackJack = true;
+        }
+        else if (sum <= 20) {
+            message = "You have " + sum + ". Do you want to draw a new card?";
+        }
+        else if (sum <= 20) {
+            message = "Sorry, you have " + sum + " . You're out.";
+            isAlive = false;
+        }
     }
     else {
-        msg += "Sorry, you have " + cardTotal + " . You're out.";
-        inGame = false;
+        if (sum === 21) {
+            message = "You have blackjack!";
+            hasBlackJack = true;
+        }
+        else {
+            message = "Sorry, you have " + sum + " . You're out.";
+            isAlive = false;
+        }
     }
-    console.log(msg);
-    messageEl.textContent = msg; // 14b
-    sumEl.textContent = "Sum:" + cardTotal; // 15b
-    cardsEl.textContent = "Cards: " + cardOne + ", " + cardTwo; // 16b
+    console.log(message);
+    messageEl.textContent = message; // 14b
 }
 
+
+/* BUTTON FUNCTIONS. Tepper, 21NOV2022 */
+// START GAME
+const startGame = () => {
+    reset();
+    generateRandomCards();
+    checkCardSums();
+}
+
+// NEW CARD
 const newCard = () => {
-    console.log("Drawing a new card from the deck.");
+    generateThirdCard();
+    checkCardSums();
 }
-
-/* FIRSTCARD FUNCTIONS. Tepper, 18NOV2022 */
-// firstCard random value generator
-const kort1 = () => {
-    let korten1 = Math.floor(Math.random() * 11) + 1;
-    firstCard = korten1;
-}
-
-// firstCard draw prompt
-const getKort1 = () => {
-    let getFirstCard = "";
-
-    getFirstCard = prompt("Draw first card? (Y/N)", "Y");
-
-    while (getFirstCard != "Y" || getFirstCard != "y") {
-        if (getFirstCard === "y" || getFirstCard === "Y") {
-            kort1();
-        }
-        else {
-            getFirstCard = prompt("Ready for your first card? (Y/N)", "Y");
-        }
-    }
-}
-
-
-/* SECONDCARD FUNCTIONS. Tepper, 18NOV2022 */
-// secondCard random value generator
-const kort2 = () => {
-    let korten2 = Math.floor(Math.random() * 11) + 1;
-    secondCard = korten2;
-}
-
-// secondCard draw prompt
-const getKort2 = () => {
-    let getSecondCard = "";
-
-    getSecondCard = prompt("Draw next card? (Y/N)", "Y");
-
-    while (getSecondCard != "Y" || getSecondCard != "y") {
-        if (getSecondCard === "y" || getSecondCard === "Y") {
-            kort2();
-        }
-        else {
-            getSecondCard = prompt("Dealer is ready. Draw next card? (Y/N)", "Y");
-        }
-    }
-}
-
-
-/* THIRDCARD FUNCTIONS. TEPPER, 21NOV2022 */
-// secondCard random value generator
-const kort3 = () => {
-    let korten3 = Math.floor(Math.random() * 11) + 1;
-    thirdCard = korten3;
-}
-
-
-// secondCard draw prompt
-const getKort3 = () => {
-    let getThirdCard = "";
-
-    getThirdCard = prompt("Do you want to hit or stay? Hit = Y, Stay = N)", "Y");
-    
-    if (getThirdCard === "y" || getThirdCard === "Y") {
-        kort3();
-        sum += thirdCard;
-    }
-}
-
-
-/* SUM FUNCTION. Tepper, 18NOV2022 */
-const cardSums = () => {
-    let pairSum = firstCard + secondCard;
-    sum = pairSum;
-}
-
-
-/* LESSON 3: if-else Conditionals
-
-Tepper, 18NOV2022 */
-
-const validateSums = () => {
-    if (sum <= 20) {
-        console.log("Do you want to draw a new card?");
-    }
-    else if (sum === 21) {
-        console.log("House folds. You've got Blackjack!");
-        hasBlackJack = true; // 7B
-    }
-    else if(sum > 21) {
-        console.log("House wins. Your cards are over 21.");
-        isAlive = false; // 8B
-    }
-}
-
-
-/* CHAINED FUNCTIONS. Tepper, 18NOV2022 */
-const blackjackDealer = () => {
-    kort1();
-    kort2(); 
-
-}
-
 
 /*************** EXTRA CHALLENGES ***************/
 /* LESSON 9. PRACTICE BOOLEAN CONDITIONS. Tepper, 21NOV2022 */
